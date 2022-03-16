a7d3e3fc8d91cbf43b7b98c0f69d746eac02a2c4
Miguel Arriaga <marriagav3@gmail.com>
2022-03-16 13:48:44 -0600

    added music sound system project in unity

---
+++ videogame-module/Sound_System_Test/Assets/Scripts/Note.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    double timeInstantiated;
+    public float assignedTime;
+    void Start()
+    {
+        timeInstantiated = SongManager.GetAudioSourceTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        double timeSinceInstantiated = SongManager.GetAudioSourceTime() - timeInstantiated;
+        float t = (float)(timeSinceInstantiated / (SongManager.Instance.noteTime * 2));
+
+        
+        if (t > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            transform.localPosition = Vector3.Lerp(Vector3.right * SongManager.Instance.noteSpawnX, Vector3.right * SongManager.Instance.noteDespawnX, t);
+
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+}
