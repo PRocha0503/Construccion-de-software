25f579c7e29ef1c7207e0db828f92ee2eb3cab5c
Miguel Arriaga <marriagav3@gmail.com>
2022-04-20 12:39:10 -0500

    organization of files

--- web-module/server/app.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require("dotenv");
-const Server = require("./models/server");
-//Exp: configurar variables de entorno
-dotenv.config();
-
-const server = new Server();
-
-server.listen();
--- web-module/server/controller/user.js
+++
@@ -1,6 +0,0 @@
-const User = require("../models/user");
-const editUser = () => {};
-
-module.exports = {
-	editUser,
-};
