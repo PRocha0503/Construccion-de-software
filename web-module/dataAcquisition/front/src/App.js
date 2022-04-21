422ca4313142cdf2c90d6f8b9ba0e536777cd924
Pablo Rocha <pablorochatmm@gmail.com>
2022-04-21 12:21:28 -0500

    style: :art: Only show section you want

--- web-module/dataAcquisition/front/src/App.js
+++ web-module/dataAcquisition/front/src/App.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import Users from "./components/Users";
 import Levels from "./components/Levels";
 import Game from "./components/Game";
 import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
 import Typography from "@mui/material/Typography";
 
 import Style from "./styles/style";
 function App() {
 	const classes = Style();
+	const [showUsers, setShowUsers] = useState(false);
+	const [showLevels, setShowLevels] = useState(false);
+	const [showGames, setShowGames] = useState(false);
+	const toggle = (set, value) => {
+		set(!value);
+	};
 	return (
 		<Box className={classes.main}>
 			<Box className={classes.container}>
@@ -14,9 +22,16 @@ function App() {
 					Endpoints Check
 				</Typography>
 			</Box>
-			<Users></Users>
-			<Levels></Levels>
-			<Game></Game>
+			<Box className={classes.container}>
+				<Button onClick={() => toggle(setShowUsers, showUsers)}>User</Button>
+				<Button onClick={() => toggle(setShowLevels, showLevels)}>
+					Levels
+				</Button>
+				<Button onClick={() => toggle(setShowGames, showGames)}>Games</Button>
+			</Box>
+			{showUsers ? <Users></Users> : <></>}
+			{showLevels ? <Levels></Levels> : <></>}
+			{showGames ? <Game></Game> : <></>}
 		</Box>
 	);
 }
