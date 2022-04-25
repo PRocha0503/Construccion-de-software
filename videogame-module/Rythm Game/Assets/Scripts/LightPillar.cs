5aee4fd2b72f93bf3c144e9b013acbeba07da134
Luisjakg <52958375+Luisjakg@users.noreply.github.com>
2022-04-24 19:28:05 -0500

    Added speakers

--- videogame-module/Rythm Game/Assets/Scripts/LightPillar.cs
+++ videogame-module/Rythm Game/Assets/Scripts/LightPillar.cs
@@ -3,6 +3,8 @@ By: Luis Javier Karam, Pablo Rocha and Miguel Arriaga
 Changes the pillars light intensity to create cool animation
 05/04/2022
 */
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,11 +14,29 @@ public class LightPillar : MonoBehaviour
 {
     [SerializeField] private new Animator light;
     [SerializeField] private Animator flame;
+    [SerializeField] private bool isSpeaker = false;
+    [SerializeField] private KeyCode input;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(input))
+        {
+            light.SetTrigger("Hit");
+        }
+
+    }
 
     public void UpdateLights(int currentMultiplier)
     {
-        light.SetInteger("Multiplier", currentMultiplier);
-        flame.SetInteger("Multiplier", currentMultiplier);
+        if (isSpeaker)
+        {
+            flame.SetInteger("Multiplier", currentMultiplier);
+        }
+        else
+        {
+            light.SetInteger("Multiplier", currentMultiplier);
+            flame.SetInteger("Multiplier", currentMultiplier);
+        }
+
     }
 }
