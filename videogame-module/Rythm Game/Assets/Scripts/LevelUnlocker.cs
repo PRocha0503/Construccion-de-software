5128d1e4a88d691b9eec6d8c15086e7230375099
Luisjakg <luisjakg@gmail.com>
2022-04-26 12:57:24 -0500

    Added Level Select Scene

---
+++ videogame-module/Rythm Game/Assets/Scripts/LevelUnlocker.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUnlocker : MonoBehaviour
+{
+    [SerializeField] private Button[] levelButtons;
+    private int levelsUnlocked;
+
+
+    private void Start()
+    {
+        for (int i = 0; i < levelsUnlocked; i++)
+        {
+            
+        }
+    }
+}
