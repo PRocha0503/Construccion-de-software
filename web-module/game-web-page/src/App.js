7340816c0c6c89ea493068c89df610ae3bcbf539
Miguel Arriaga <marriagav3@gmail.com>
2022-05-03 13:00:31 -0500

    pretty website

--- web-module/game-web-page/src/App.js
+++ web-module/game-web-page/src/App.js
@@ -1,15 +1,42 @@
 import "./App.css";
 import UnityGame from "./components/UnityGame";
+import MusicSheet from "./components/MusicSheet";
+import { Typography, Box } from "@material-ui/core";
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "VT323",
+      "Roboto",
+      "Helvetica Neue",
+      "Arial",
+      "sans-serif",
+    ].join(","),
+  },
+});
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>Unity Game</h1>
-				<UnityGame></UnityGame>
-			</header>
-		</div>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          backgroundColor: "#AEA9BA",
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+          alignItems: "center",
+          justifyContent: "center",
+          alignText: "center",
+          color: "#6959a2",
+        }}
+      >
+        <Typography variant="h1">Unity Game</Typography>
+        <UnityGame></UnityGame>
+      </Box>
+      <MusicSheet></MusicSheet>
+    </ThemeProvider>
+  );
 }
 
 export default App;
